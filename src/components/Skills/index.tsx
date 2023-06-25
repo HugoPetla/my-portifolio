8b40ffa31324d15ebacb310880b1f4f98cb5effd
Hugo Petla <hugo.petla.ext@dasa.com.br>
2023-06-25 20:44:10 -0300

    Changed the skills to array

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+const skillsList = [
+  { name: "Web", positionX: "0xw", positionY: "0vw" },
+  { name: "CSS", positionX: "-5vw", positionY: "-10vw" },
+  { name: "HTML", positionX: "-20vw", positionY: "2vw" },
+  { name: "Javascript", positionX: "20vw", positionY: "6vw" },
+  { name: "ReactJS", positionX: "0vw", positionY: "7vw" },
+  { name: "NextJS", positionX: "10vw", positionY: "4vw" },
+  { name: "Web Design", positionX: "32vw", positionY: "-5vw" },
+  { name: "Figma", positionX: "0vw", positionY: "-20vw" },
+  { name: "Tailwind", positionX: "10vw", positionY: "-15vw" },
+  { name: "MUI", positionX: "10vw", positionY: "-5vw" },
+  { name: "Jest", positionX: "-25vw", positionY: "15vw" },
+];
+
 const Skill = ({ name, x, y }: { name: string; x: string; y: string }) => {
   return (
     <>
@@ -22,17 +36,14 @@ export const Skills = () => {
     <div className="w-full my-32">
       <h2 className="font-bold text-8xl w-full text-center">Skills</h2>
       <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
-        <Skill name="Web" x="0vw" y="0vw" />
-        <Skill name="CSS" x="-5vw" y="-10vw" />
-        <Skill name="HTML" x="-20vw" y="2vw" />
-        <Skill name="Javascript" x="20vw" y="6vw" />
-        <Skill name="ReactJS" x="0vw" y="12vw" />
-        <Skill name="NextJS" x="-20vw" y="-15vw" />
-        <Skill name="Web Design" x="32vw" y="-5vw" />
-        <Skill name="Figma" x="0vw" y="-20vw" />
-        <Skill name="Tailwind" x="10vw" y="-15vw" />
-        <Skill name="MUI" x="10vw" y="-5vw" />
-        <Skill name="Jest" x="-25vw" y="15vw" />
+        {skillsList.map((skill, index) => (
+          <Skill
+            name={skill.name}
+            x={skill.positionX}
+            y={skill.positionY}
+            key={index}
+          />
+        ))}
       </div>
     </div>
   );
